16793cd57f9170008b79d23c70f090ad0cb87fe9
Poseyd29 <poseyd29@gmail.com>
2022-05-08 23:27:22 -0400

    refreshed
    uploaded new version of portfolio

---
+++ portfolio/js/main.js
@@ -0,0 +1,12 @@
+.zoomIn {
+    /* animation for feature card */
+    transition: transform .2s;
+    /* max-width: 25%; */
+    width: 350px;
+}
+
+.zoomIn: hover {
+    transform: scale(1.1);
+    box - shadow: 0px 1px 4px 2px #b63232;
+    cursor: pointer;
+}
