5b715886304559b8c20b89c72c4e936a7d63fd9e
Poseyd29 <poseyd29@gmail.com>
2022-06-03 21:06:23 -0400

    style updates
    showcase added + img container adjusted

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -10,3 +10,24 @@
     box - shadow: 0px 1px 4px 2px #b63232;
     cursor: pointer;
 }
+
+
+
+
+
+
+
+// filter method using traditional syntax
+let nums = [1, -3, 4, 6];
+
+const filtered = nums.filter(function (value) {
+    return value >= 0;
+})
+
+// filter method using ES6 arrow function syntax
+
+
+
+
+const filtered = nums.filter(value => value >= 0);
+
